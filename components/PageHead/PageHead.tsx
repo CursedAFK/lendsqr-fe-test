0bd3af838a4035dc2318b30d3371d801af9dd388
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-05-06 15:48:09 +0100

    done v2

--- components/PageHead/PageHead.tsx
+++ components/PageHead/PageHead.tsx
@@ -20,6 +20,22 @@ const PageHead = ({ pageTitle }: Props) => {
 				name='robots'
 				content='index, follow'
 			/>
+			<meta
+				property='og:title'
+				content={pageTitle}
+			/>
+			<meta
+				property='og:description'
+				content='Lendsqr is a platform used by over half a million lenders to provide loans to customers. Our frontend software stack is built with React, TypeScript and SCSS, allowing us to ideate and release features rapidly and reliably. This web app showcases our frontend engineering capabilities, with a focus on visual fidelity, code quality, and best practices in design and architecture. The app includes login, dashboard, user page, and user details page, all of which pull data from a mock API with 500 records. Designed to be mobile responsive, this app is a testament to Lendsqr commitment to attention to detail and meeting acceptance criteria. Try it out now!'
+			/>
+			<meta
+				property='og:image'
+				content='https://abdulramon-lasisi-lendsqr-fe-test.vercel.app/images/preview.png'
+			/>
+			<meta
+				property='og:url'
+				content='https://abdulramon-lasisi-lendsqr-fe-test.vercel.app/'
+			/>
 			<link
 				rel='icon'
 				href='logo.png'
